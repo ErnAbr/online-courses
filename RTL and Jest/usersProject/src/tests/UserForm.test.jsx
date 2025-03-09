0bd39e35329c5572ed83a7641381dc5104c11fa9
ErnAbr <ern_abr@yahoo.com>
2025-03-09 20:50:41 +0200

    testing form input and submision

--- RTL and Jest/usersProject/src/tests/UserForm.test.jsx
+++ RTL and Jest/usersProject/src/tests/UserForm.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import user from "@testing-library/user-event";
+import userEvent from "@testing-library/user-event";
 import { UserForm } from "../components/UserForm";
 
 test("it shows two inputs and a button", () => {
@@ -11,4 +11,33 @@ test("it shows two inputs and a button", () => {
   // Assertion - make sure the component is doing what we expect to do
   expect(inputs).toHaveLength(2);
   expect(button).toBeInTheDocument();
+});
+
+test("it calls onUserAdd when the form is submitted", async () => {
+  const argList = [];
+  const callback = (...args) => {
+    argList.push(args);
+  };
+
+  // Render the component
+  render(<UserForm onUserAdd={callback} />);
+
+  // Find the two inputs
+  const [nameInput, emailInput] = screen.getAllByRole("textbox");
+
+  // Simulate typing in a name
+  await userEvent.type(nameInput, "jane");
+
+  // Simulate typing in an email
+  await userEvent.type(emailInput, "jane@gmail.com");
+
+  // Find the button
+  const button = screen.getByRole("button");
+
+  // Simulate clicking the button
+  await userEvent.click(button);
+
+  // Assertion to make sure 'onUserAdd' gets called with email/name
+  expect(argList).toHaveLength(1);
+  expect(argList[0][0]).toEqual({ name: "jane", email: "jane@gmail.com" });
 });
