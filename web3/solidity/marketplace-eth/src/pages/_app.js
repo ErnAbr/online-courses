7dfca2a305527fb65d0a4f67b2d522538f31df3f
ErnAbr <ern_abr@yahoo.com>
2025-08-02 14:57:19 +0300

    nextjs marketplace-eth app layout build

---
+++ web3/solidity/marketplace-eth/src/pages/_app.js
@@ -0,0 +1,13 @@
+import "@styles/globals.css";
+
+const Noop = ({ children }) => <>{children}</>;
+
+export default function App({ Component, pageProps }) {
+  const Layout = Component.Layout ?? Noop;
+
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
---
+++ web3/solidity/marketplace-eth/src/pages/index.js
@@ -0,0 +1,20 @@
+import { Hero, BreadCrumbs } from "@components/common";
+import { CourseList } from "@components/course";
+import { BaseLayout } from "@components/layout";
+import { OrderCard } from "@components/order";
+import { EthRates, WalletBar } from "@components/web3";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <BreadCrumbs />
+      <WalletBar />
+      <EthRates />
+      <OrderCard />
+      <CourseList />
+    </>
+  );
+}
+
+Home.Layout = BaseLayout;
