36f178aa70d0a1bf8cd93c6c91e97e22acdca983
ErnAbr <ern_abr@yahoo.com>
2025-08-11 21:08:38 +0300

    web3 provider initial setup

--- web3/solidity/marketplace-eth/src/pages/index.js
+++ web3/solidity/marketplace-eth/src/pages/index.js
@@ -1,11 +1,16 @@
-import { Hero } from "@components/common";
-import { CourseList } from "@components/course";
-import { BaseLayout } from "@components/layout";
+import { useWeb3 } from "@components/providers";
+import { Hero } from "@components/ui/common";
+import { CourseList } from "@components/ui/course";
+import { BaseLayout } from "@components/ui/layout";
 import { getAllCourses } from "@content/courses/fetcher";
 
 export default function Home({ courses }) {
+  const { web3, isInitialized } = useWeb3();
+  console.log(web3);
+
   return (
     <>
+      {isInitialized ? "is init" : "is not init"}
       <Hero />
       <CourseList courses={courses} />
     </>
