79fcfbc5cacb234bb43bd3b9cdede22597093148
ErnAbr <ern_abr@yahoo.com>
2025-09-12 21:01:48 +0300

    course card styling

--- web3/solidity/marketplace-eth/src/components/ui/course/card/index.js
+++ web3/solidity/marketplace-eth/src/components/ui/course/card/index.js
@@ -1,30 +1,33 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Card({ course }) {
+export default function Card({ course, Footer }) {
   return (
-    <div className="bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl flex flex-col sm:flex-row">
-      <div className="w-full sm:w-[200px] h-[230px] relative shrink-0">
+    <div className="bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl flex ">
+      <div className="relative w-36 h-auto flex-shrink-0">
         <Image
           src={course.coverImage}
           alt={course.title}
           fill
           className="object-cover"
-          sizes="(max-width: 768px) 100vw, 50vw"
+          sizes="100vw"
         />
       </div>
 
-      <div className="p-6 flex flex-col justify-center">
+      <div className="p-6 pb-2 flex flex-col justify-center">
         <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
           {course.type}
         </div>
         <Link
           href={`/courses/${course.slug}`}
-          className="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
+          className="h-12 block mt-1 text-lg leading-tight font-medium text-black hover:underline"
         >
           {course.title}
         </Link>
-        <p className="mt-2 text-gray-500">{course.description}</p>
+        <p className="mt-2 text-gray-500">
+          {course.description.substring(0, 70)}...
+        </p>
+        {Footer && <Footer />}
       </div>
     </div>
   );
