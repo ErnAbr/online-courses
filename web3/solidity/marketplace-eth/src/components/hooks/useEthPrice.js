75530b3cf34468662d44d5892b88fc3e2a632254
ErnAbr <ern_abr@yahoo.com>
2025-10-10 21:36:38 +0300

    getting app running again

--- web3/solidity/marketplace-eth/src/components/hooks/useEthPrice.js
+++ web3/solidity/marketplace-eth/src/components/hooks/useEthPrice.js
@@ -16,7 +16,7 @@ export const useEthPrice = () => {
     refreshInterval: 10000,
   });
 
-  const perItem = (data && COURSE_PRICE / Number(data)).toFixed(6) ?? null;
+  const perItem = data ? (COURSE_PRICE / Number(data)).toFixed(6) : null;
 
   return {
     eth: { data, perItem, ...rest },
