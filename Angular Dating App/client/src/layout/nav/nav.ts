1103f9832dc9748c43144900649e9e3e10aa13eb
ErnAbr <ern_abr@yahoo.com>
2025-11-19 20:58:09 +0200

    toast-service

--- Angular Dating App/client/src/layout/nav/nav.ts
+++ Angular Dating App/client/src/layout/nav/nav.ts
@@ -1,28 +1,36 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastService } from '../../core/services/toast-service';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
 export class Nav {
+  private router = inject(Router);
+  private toastService = inject(ToastService);
   protected accountService = inject(AccountService);
   protected creds: any = {};
 
   login() {
     this.accountService.login(this.creds).subscribe({
       next: (result) => {
-        console.log(result);
+        this.router.navigateByUrl('/members');
+        this.toastService.success('Logged in Successfully');
         this.creds = {};
       },
-      error: (error) => alert(error.message),
+      error: (error) => {
+        this.toastService.error(error.error);
+      },
     });
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
